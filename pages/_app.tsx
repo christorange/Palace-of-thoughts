055efc94d290b0116f6a66bdcd7feabcfe394f62
christorange <christol.yu@outlook.com>
2023-12-07 21:33:38 -0500

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,24 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { cn } from '@/lib/utils'
+import { Julius_Sans_One } from 'next/font/google'
+import Head from 'next/head'
+
+const fontJulius = Julius_Sans_One({
+  weight: '400',
+  subsets: ['latin'],
+  variable: '--font-primary'
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Filter of Inspirations</title>
+      </Head>
+      <body className={cn(fontJulius.variable)}>
+        <Component {...pageProps} />
+      </body>
+    </>
+  )
 }
